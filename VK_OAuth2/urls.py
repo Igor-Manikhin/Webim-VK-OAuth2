389f1195c766ca11de44e26e6d7dfa96e6b4094e
Igor <imanihin822@gmail.com>
2020-07-20 22:56:56 +0300

    added gunicorn config

--- VK_OAuth2/urls.py
+++ VK_OAuth2/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from User_Profile import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('webim-oauth-admin/', admin.site.urls),
     path('login/', views.index, name="Login"),
     path('logout/', views.logout_view, name="Logout"),
     path('profile/', views.profile_view, name="Profile"),
